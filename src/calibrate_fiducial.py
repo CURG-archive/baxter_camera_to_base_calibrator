642a664e7be01fbfbbdaa47882ec8451ef853b54
jvarley <jakevarley@gmail.com>
2014-09-10 19:30:32 -0400

    mico-branch

--- src/calibrate_fiducial.py
+++ src/calibrate_fiducial.py
@@ -2,7 +2,7 @@ import tf
 import tf_conversions
 import rospy
 import rosbag
-import pdb
+import ipdb
 import numpy
 import scipy
 import scipy.optimize
@@ -38,7 +38,7 @@ def get_error(checkerboard_to_wrist_estimate, camera_in_body_estimate, checkerbo
         error = camera_checkerboard_in_body - checkerboard_in_body
         total_error += sum(sum(abs(error)))
         if run_pdb:
-            pdb.set_trace()
+            ipdb.set_trace()
     return total_error
 
 def get_total_error(parameters, checkerboard_in_camera_trans, wrist_in_body_trans):    
@@ -84,12 +84,12 @@ def get_transform_lists(bag_file_name):
                 tf_child_ids.add(tf_message.child_frame_id)
                 print 'found new child frame %s' % tf_message.child_frame_id
 
-            if 'wrist_board' in tf_message.header.frame_id or 'wrist_board_corner' in tf_message.child_frame_id:
+            if 'wrist_board' in tf_message.child_frame_id:
                 print 'found keyframe'
                 if camera_in_body_estimate is None:
                     try:
-                        listener.waitForTransform('/camera_link','/world',rospy.Time(0), rospy.Duration(.01))
-                        camera_in_body_tf = listener.lookupTransform('/camera_link','/world',rospy.Time(0))
+                        listener.waitForTransform('/camera_link','/root',rospy.Time(0), rospy.Duration(.01))
+                        camera_in_body_tf = listener.lookupTransform('/camera_link','/root',rospy.Time(0))
                         
                         print "got camera to world transform"
                         camera_in_body_estimate = tf_conversions.toMatrix(tf_conversions.fromTf(camera_in_body_tf))
@@ -100,11 +100,11 @@ def get_transform_lists(bag_file_name):
 
                 if checkerboard_to_wrist_estimate is None:
                     try:
-                        listener.waitForTransform('/wrist_board','/wrist_board_corner',rospy.Time(0), rospy.Duration(.01))
+                        listener.waitForTransform('/mico_link_hand','/wrist_board',rospy.Time(0), rospy.Duration(.01))
                         #(trans, rot) = listener.lookupTransform('/wrist_board','/wrist_board_corner',rospy.Time(0))
                         #print trans
                         #print rot
-                        checkerboard_to_wrist_tf = listener.lookupTransform('/wrist_board','/wrist_board_corner',rospy.Time(0))
+                        checkerboard_to_wrist_tf = listener.lookupTransform('/wrist_board','/mico_link_hand',rospy.Time(0))
                         #print checkerboard_to_wrist_tf
                         #raw_input("press a key")
 
@@ -120,7 +120,7 @@ def get_transform_lists(bag_file_name):
                     print "got wristboard in wrist estimate"
                 try:
                     listener.waitForTransform('/wrist_board','/camera_link',rospy.Time(0), rospy.Duration(.01))
-                    listener.waitForTransform('/wrist_board_corner','/world',rospy.Time(0), rospy.Duration(.1))
+                    listener.waitForTransform('/wrist_board','/root',rospy.Time(0), rospy.Duration(.1))
 
                     checkerboard_tf = listener.lookupTransform('/wrist_board','/camera_link',rospy.Time(0))
                     #print "got wristboard in camera"
@@ -128,7 +128,7 @@ def get_transform_lists(bag_file_name):
                     checkerboard_in_camera_trans.append(tf_conversions.toMatrix(tf_conversions.fromTf(checkerboard_tf)))
 
                     #print "got left wrist in world"
-                    wrist_in_body_tf = listener.lookupTransform('/wrist_board_corner','/world',rospy.Time(0))
+                    wrist_in_body_tf = listener.lookupTransform('/mico_link_hand','/root',rospy.Time(0))
                     wrist_in_body_trans.append(tf_conversions.toMatrix(tf_conversions.fromTf(wrist_in_body_tf)))
                 except:
                     continue
@@ -151,17 +151,19 @@ def estimate_kinect_to_base(bag_file_name):
         wrist_in_body_trans = npzfile['wrist_in_body_trans']
         camera_in_body_estimate = npzfile['camera_in_body_estimate']
         checkerboard_to_wrist_estimate = npzfile['checkerboard_to_wrist_estimate']
+        ipdb.set_trace()
         print 'loaded data from file'
     except:
         print 'generating numpy file'
         checkerboard_in_camera_trans, wrist_in_body_trans, camera_in_body_estimate, checkerboard_to_wrist_estimate = get_transform_lists(bag_file_name)
+        ipdb.set_trace()
         numpy.savez(numpy_file_name, checkerboard_in_camera_trans = checkerboard_in_camera_trans, wrist_in_body_trans = wrist_in_body_trans, camera_in_body_estimate = camera_in_body_estimate, checkerboard_to_wrist_estimate = checkerboard_to_wrist_estimate) 
 
 
     checkerboard_tran_params = matrix_to_parameter_descriptor(checkerboard_to_wrist_estimate)
     camera_tran_params = matrix_to_parameter_descriptor(camera_in_body_estimate)
     parameters = numpy.hstack([checkerboard_tran_params, camera_tran_params])    
-    retval = scipy.optimize.fmin_powell(get_total_error, parameters, args=(checkerboard_in_camera_trans, wrist_in_body_trans), xtol=0.001, ftol=0.001, maxiter=10000, maxfun=10000, full_output=0, disp=1, retall=0, callback=None, direc=None)
+    retval = scipy.optimize.fmin_powell(get_total_error, parameters, args=(checkerboard_in_camera_trans, wrist_in_body_trans), xtol=0.0003, ftol=0.0003, maxiter=10000, maxfun=10000, full_output=0, disp=1, retall=0, callback=None, direc=None)
     kinect_to_base = parameter_descriptor_to_mat(retval[6:])
     return  tf.transformations.translation_from_matrix(kinect_to_base), tf.transformations.quaternion_from_matrix(kinect_to_base)
 
@@ -175,7 +177,7 @@ def main():
     arr1 = str1[1:-1].split()
     arr2 = str2[1:-1].split()
 
-    print 'args="' + ', '.join(arr1 + arr2) + ' /camera_link /world 100"'
+    print 'args="' + ', '.join(arr1 + arr2) + ' /camera_link /root 100"'
 
 if __name__ == "__main__":
     main()
